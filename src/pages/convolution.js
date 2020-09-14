aff95eb1ec5d8ba323ac7d8e8c355c815451e95a
Miller Correa <macorreag@unal.edu.co>
2020-09-14 08:51:05 -0500

    Revert "merge [p5 and cards in project]"
    This reverts commit 3b55dcb6bbbe14c1068d82f0bc0fc6581013dfe2, reversing
    changes made to 0e5f2a90d6f03ac2c0242f5b5ac6cb7b29d4a998.

---
+++ src/pages/convolution.js
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/app.css"
+import React from "react"
+export default () => {
+  return (
+    <div className="jumbotron m-4 bg-gradient-orange">
+      <h1 className="display-4">Convoluciones con P5</h1>
+      <p className="lead">lorem ipsum</p>
+      <hr className="my-4"></hr>
+      <p>lorem ipsum</p>
+      <a className="btn btn-lg btn-dark" href="#" role="button">
+        Convolución
+      </a>
+    </div>
+  )
+}
