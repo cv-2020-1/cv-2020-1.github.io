bbf6b98ea1e03fbd18ce07b3016038070c170539
Miller Correa <macorreag@unal.edu.co>
2020-09-22 20:38:32 -0500

    Change path for fix problem in Linux

--- src/pages/ilusions.js
+++ src/pages/ilusions.js
@@ -1,25 +1,32 @@
+import React from "react"
+import CafeWall from "../algorithms/Illusions/cafeWall"
 
-import React from "react";
-import CafeWall from '../algorithms/illusions/cafeWall.js';
-import Ebbinghaus from '../algorithms/illusions/Ebbinghaus';
- 
+import Ebbinghaus from "../algorithms/Illusions/ebbinghaus"
+
 export default () => {
-
-    return(
-      <div className="ml-5 mr-5 my-3">
-        <div>
-          <h1>Ilusiones</h1>
-          <h2>Cafe Wall</h2>
-          <p>La ilusión de la pared de la cafetería es un tipo de ilusión óptico-geométrica, en la que líneas rectas paralelas  que dividien líneas entre 
-            filas formadas por baldosas blancas y negras alternas y escalonadas, aparentan estar inclinadas.</p>
-          <CafeWall />
-          <h2>Ebbinghaus</h2>
-          <p>La ilusión de Ebbinghaus es una ilusión óptica que altera la percepción de las dimensiones relativas. 
-            En la versión más conocida de la ilusión, dos círculos de la misma medida son colocados cercanos uno a otro 
-            y son circundados, uno por círculos de un tamaño mayor y el otro por círculos de menor tamaño; el primer 
-            círculo central parecerá más pequeño que el otro</p>
-          <Ebbinghaus />
-        </div>
+  return (
+    <div className="ml-5 mr-5 my-3">
+      <div>
+        <h1>Ilusiones</h1>
+        <h2>Cafe Wall</h2>
+        <p>
+          La ilusión de la pared de la cafetería es un tipo de ilusión
+          óptico-geométrica, en la que líneas rectas paralelas que dividien
+          líneas entre filas formadas por baldosas blancas y negras alternas y
+          escalonadas, aparentan estar inclinadas.
+        </p>
+        <CafeWall />
+        <h2>Ebbinghaus</h2>
+        <p>
+          La ilusión de Ebbinghaus es una ilusión óptica que altera la
+          percepción de las dimensiones relativas. En la versión más conocida de
+          la ilusión, dos círculos de la misma medida son colocados cercanos uno
+          a otro y son circundados, uno por círculos de un tamaño mayor y el
+          otro por círculos de menor tamaño; el primer círculo central parecerá
+          más pequeño que el otro
+        </p>
+        <Ebbinghaus />
       </div>
-    )
-};
+    </div>
+  )
+}
