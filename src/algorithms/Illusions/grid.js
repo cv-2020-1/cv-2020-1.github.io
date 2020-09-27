b1c68c6eac511eb757a622eb2c2e55b4c92af4d7
Brayan Hurtado <bohurtadop@unal.edu.co>
2020-09-27 14:21:55 -0500

    Grid illusion

---
+++ src/algorithms/Illusions/grid.js
@@ -0,0 +1,50 @@
+
+import React from "react";
+import loadable from "@loadable/component"
+
+
+export default function Grid () {
+
+    function setup(p5, canvasParentRef) {
+        p5.createCanvas(400, 400).parent(canvasParentRef);
+        p5.strokeWeight(3);        // medium weight lines
+        p5.smooth();               // antialias lines
+        p5.stroke(100, 100, 100);  // dark grey colour for lines
+        p5.noLoop();
+    
+    }
+    
+    function draw(p5) {
+        p5.background(0);          // black background
+        var step = 25;          // grid spacing
+    
+        //vertical lines
+        for (var x = step; x < p5.width; x = x + step) {
+            p5.line(x, 0, x, p5.height);
+        }
+    
+        //horizontal lines
+        for (var y = step; y < p5.height; y = y + step) {
+            p5.line(0, y, p5.width, y);
+        }
+    
+        // Circles
+        p5.ellipseMode(p5.CENTER);
+        p5.stroke(255, 255, 255);  // white circles
+        for (var i = step; i < p5.width -5; i = i + step) {
+            for (var j = step; j < p5.height -15; j = j + step) {
+                p5.strokeWeight(6);
+                p5.point(i, j);
+                p5.strokeWeight(3);
+            }
+        }
+    }
+    
+    if (typeof window !== "undefined") {
+      const Sketch = loadable(() => import("react-p5"))
+      return <Sketch setup={setup} draw={draw} />;
+    }
+    else {
+      return null
+    }
+};
--- src/pages/ilusions.js
+++ src/pages/ilusions.js
@@ -1,32 +1,41 @@
-import React from "react"
-import CafeWall from "../algorithms/Illusions/cafeWall"
-
-import Ebbinghaus from "../algorithms/Illusions/ebbinghaus"
+import React from "react";
+import CafeWall from "../algorithms/Illusions/cafeWall";
+import Ebbinghaus from "../algorithms/Illusions/ebbinghaus";
+import Grid from "../algorithms/Illusions/grid";
 
 export default () => {
-  return (
-    <div className="ml-5 mr-5 my-3">
-      <div>
-        <h1>Ilusiones</h1>
-        <h2>Cafe Wall</h2>
-        <p>
-          La ilusión de la pared de la cafetería es un tipo de ilusión
-          óptico-geométrica, en la que líneas rectas paralelas que dividien
-          líneas entre filas formadas por baldosas blancas y negras alternas y
-          escalonadas, aparentan estar inclinadas.
-        </p>
-        <CafeWall />
-        <h2>Ebbinghaus</h2>
-        <p>
-          La ilusión de Ebbinghaus es una ilusión óptica que altera la
-          percepción de las dimensiones relativas. En la versión más conocida de
-          la ilusión, dos círculos de la misma medida son colocados cercanos uno
-          a otro y son circundados, uno por círculos de un tamaño mayor y el
-          otro por círculos de menor tamaño; el primer círculo central parecerá
-          más pequeño que el otro
-        </p>
-        <Ebbinghaus />
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="ml-5 mr-5 my-3">
+			<div>
+				<h1>Ilusiones</h1>
+				<h2>Cafe Wall</h2>
+				<p>
+					La ilusión de la pared de la cafetería es un tipo de ilusión
+					óptico-geométrica, en la que líneas rectas paralelas que dividien
+					líneas entre filas formadas por baldosas blancas y negras alternas y
+					escalonadas, aparentan estar inclinadas.
+				</p>
+				<CafeWall />
+				<h2>Ebbinghaus</h2>
+				<p>
+					La ilusión de Ebbinghaus es una ilusión óptica que altera la
+					percepción de las dimensiones relativas. En la versión más conocida de
+					la ilusión, dos círculos de la misma medida son colocados cercanos uno
+					a otro y son circundados, uno por círculos de un tamaño mayor y el
+					otro por círculos de menor tamaño; el primer círculo central parecerá
+					más pequeño que el otro
+				</p>
+				<Ebbinghaus />
+				<h2>Grid</h2>
+				<p>
+					La ilusión de cuadrícula de Hermann es una ilusión óptica descrita por
+					Ludimar Hermann en 1870. La ilusión se caracteriza por manchas grises
+					"fantasmales" que se perciben en las intersecciones de una cuadrícula
+					blanca (o de color claro) sobre un fondo negro. Las manchas grises
+					desaparecen al mirar directamente a una intersección.
+				</p>
+				<Grid />
+			</div>
+		</div>
+	);
+};
