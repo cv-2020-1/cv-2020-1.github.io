04128b431614a592c15d5d65befd893db94ee538
daandradec <daandradec@unal.edu.co>
2020-10-03 21:57:36 -0500

    Video checkboxes funcional

--- src/algorithms/video/VideoPlayer.js
+++ src/algorithms/video/VideoPlayer.js
@@ -15,56 +15,60 @@ import {___, ____, _____, EdgeDetectionKernel} from '../../algorithms/convolutio
 
 
 
-export default (props) => {
-	let playing = false;
+export default (props) => {	
 	let size = 45;
 	let height = 9*size;
   	let width = 16*size;  
-	let video;
-	let button;
-
-	let state = 0;
+	let video;		
 
 	const setup = (p5, canvasParentRef) => {		
 		p5.createCanvas(width*2,height).parent(canvasParentRef);
 		video = p5.createVideo([videomp4, videowebm]);
 		video.parent(canvasParentRef);
 		video.size(width, height);
-		video.hide()
+		
+		// Para no tener que esconder el video original y facilitar el trabajo a la cpu
+		video.elt.parentElement.classList.add("position-relative");
+		video.elt.classList.add("position-absolute");
+		video.elt.style.top = "0";
+		video.elt.style.left = "0";
+		//video.hide()
 
-		setTimeout(function(){
-			video.pause();
+		setTimeout(function(){			
 			video.play()
-			video.loop();
+			video.loop();			
 		}, 500);
+		
 	};
 
 	const draw = (p5) => {
 		p5.background(255);	
 		// MAQUINA DE ESTADOS
-		switch(state){
+
+		switch(props.state){
 			case 0: // FILTRO GRIS	
 				p5.push();
-				p5.image(video.size(width,height), width, 0, width, height);
+				p5.image(video.size(width,height), width, 0, width,height);
 				p5.filter(p5.GRAY);
 				p5.pop();
 				break;
 			case 1:// CONVOLUCION DETECTOR DE BORDES
 				video.loadPixels();
-				p5.image(convolution(p5, video, EdgeDetectionKernel),width,0, p5.width, p5.height);
+				p5.image(convolution(p5, video, EdgeDetectionKernel),width,0, width,height);
 				break;
 			case 2:// ESCALA DE GRISES CON BRILLO
 				video.loadPixels();
-				p5.image(editPixels(p5, video, "average"),width,0, p5.width, p5.height);
+				p5.image(editPixels(p5, video, "average"),width,0, width, height);
 				break;
 			default:
-				p5.image(video, width, 0, p5.width, p5.height);
+				p5.image(video.size(width,height), width, 0, width,height);
 				break;
 		}
 
-		p5.push();	
-		p5.image(video.size(width,height), 0, 0, width, height);				
-		p5.pop();							
+		// Dibujado del video normal pero ya no se usa porque consume cpu y se pone lento, 
+		//p5.push();	
+		//p5.image(video.size(width,height), 0, 0, width,height);				
+		//p5.pop();	
 	};
 
 	if (typeof window !== "undefined") {
--- src/pages/video.js
+++ src/pages/video.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import VideoPlayer from "../algorithms/video/VideoPlayer";
 
 export default () => {
+
+	const [state, setValue] = useState(0); // integer state	
+
+	const submitForm = (e) => {
+		e.preventDefault();
+	}
+
+	const radioChange = (param, e) => {
+		switch(param){
+			case 'r1': setValue(0); /*state = 0;*/ break;
+			case 'r2': setValue(1); /*state = 1;*/ break;
+			case 'r3': setValue(2); /*state = 2;*/ break;
+			case 'r4': setValue(3); /*state = 3;*/ break;
+		}		
+	}
+
 	return (
 		<div className="ml-5 mr-5 my-3">
 			<div>
 				<h1>Test video</h1>
-				<VideoPlayer />
-				<form action="">
-					<input type="checkbox" value="checked"/>
+				<VideoPlayer state={state}/>
+				<form onSubmit={submitForm}>
+					<input type="radio" className="mx-3" id="r1" name="group1" value="r1" onChange={radioChange.bind(this,'r1')}/>
+					<label htmlFor="r1">FILTRO GRIS</label>
+					<input type="radio" className="mx-3" id="r2" name="group1" value="r2" onChange={radioChange.bind(this,'r2')}/>
+					<label htmlFor="r2">CONVOLUCION DETECTOR BORDES</label>
+					<input type="radio" className="mx-3" id="r3" name="group1" value="r3" onChange={radioChange.bind(this,'r3')}/>
+					<label htmlFor="r3">ESCALA GRISES</label>
+					<input type="radio" className="mx-3" id="r4" name="group1" value="r4" onChange={radioChange.bind(this,'r4')}/>
+					<label htmlFor="r4">VIDEO ORIGINAL</label>					
 				</form>
 			</div>
 		</div>
