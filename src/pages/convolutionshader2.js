cb7f11c8229d8bc1f59c495438a33498f5815c36
daandradec <daandradec@unal.edu.co>
2020-10-25 22:06:02 -0500

    iframe

--- src/pages/convolutionshader2.js
+++ src/pages/convolutionshader2.js
@@ -1,47 +1,29 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 import React from "react"
-import p5 from "p5"
-import fuego from "../assets/fuego.jpg";
+import loadable from "@loadable/component"
 
 
-let img;
-let shader;
+export default (props) => {
 
-const page = (props) => {
+  const setup = (p5, canvasParentRef) => {       
+    p5.createCanvas(500, 300).parent(canvasParentRef);    
+    p5.noStroke();
+  }
 
-    const sketch = (p) => {
+  const draw = (p5) => {
+    p5.rect(0,0,200, 200);
+  }
 
-        p.preload = () => {
-            shader = p.loadShader('../shaders/convolution/vertex.vert', '../shaders/convolution/fragment.frag');
-            img = p.loadImage(fuego);
-        }
-        
-        p.setup = () => {
-            let canvas = p.createCanvas(500, 300, p.WEBGL);
-            p.noStroke();
-        }
-        
-        p.draw = () => {
-            p.shader(shader);
-            shader.setUniform('tex0', img);
-            shader.setUniform('stepSize', [1.0 / 500, 1.0 / 300]);
-            shader.setUniform('dist', 3.0);
-            p.rect(0,0,200, 200);
-        }    
-    }
   if (typeof window !== "undefined") {
-    const p5canvas = new p5(sketch,"p5")
+    const Sketch = loadable(() => import("react-p5"))
     return (
-      <div className="ml-5 mr-5 my-3">         
+      <div className="ml-5 mr-5 my-3">
         <h1>MASCARAS DE CONVOLUCIÓN SHADERS</h1>
-        <img src={fuego} id="img" width="300px"></img>
-        <div id="p5">
-        </div>                
+        <Sketch setup={setup} draw={draw}/>
+        <p>Utilizando un iframe</p>
+        <iframe src="https://cv-2020-1.github.io/Shaders/pages/convolution.html" style={{overflow:'hidden'}}></iframe>
       </div>
     )
   } else
     return null
 }
-
-
-export default page;
