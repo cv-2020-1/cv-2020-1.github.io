60c7d6669db433e4ba1079f9d46ed34d9fa00e5d
jspaeza <jspaeza@gmail.com>
2020-11-01 19:47:10 -0500

    ref

--- src/algorithms/Illusions/ebbinghaus.js
+++ src/algorithms/Illusions/ebbinghaus.js
@@ -15,10 +15,10 @@ export default function Ebbinghaus() {
         let k = p5.map(p5.mouseX, 0, p5.width, 200, 0)
     
         p5.noStroke()
-        p5.fill('yellow')
+        p5.fill('red')
         p5.ellipse(160, 200, 50, 50)
     
-        p5.fill(155, 0, 215, k)
+        p5.fill(200, 150, 215, k)
     
         p5.ellipse(110, 120, 90, 90)
         p5.ellipse(210, 120, 90, 90)
@@ -29,10 +29,10 @@ export default function Ebbinghaus() {
         p5.ellipse(60, 200, 90, 90)
         p5.ellipse(260, 200, 90, 90)
     
-        p5.fill('yellow')
+        p5.fill('red')
         p5.ellipse(450, 200, 50, 50)
     
-        p5.fill(155, 0, 215, k)
+        p5.fill(200, 150, 215, k)
     
         p5.ellipse(450, 154, 25, 25)
         p5.ellipse(450, 246, 25, 25)
